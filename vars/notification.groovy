f136711e11957fbfc0e7477497a0a6b5b8bd5b89
Rishabap <91373260+Rishabap@users.noreply.github.com>
2025-01-21 15:48:38 +0530

    Update notification.groovy

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -1,1 +1,7 @@
-
+def notification(){
+  failure {
+        mail to: 'rishabap23@gmail.com',
+             subject: "Pipeline Execution: ${currentBuild.fullDisplayName}",
+             body: "The build ${currentBuild.fullDisplayName} has completed with status: ${currentBuild.result}."
+    }
+}
