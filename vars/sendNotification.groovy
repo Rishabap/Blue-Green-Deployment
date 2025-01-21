2d3a8b6c903d7afe1a3fd486b0ffac7238c60b61
Rishabap <91373260+Rishabap@users.noreply.github.com>
2025-01-21 18:20:15 +0530

    Update sendNotification.groovy

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -1,6 +1,6 @@
 
 def sendnotification() {
-        if (currentBuild.result == 'FAILURE')
+        if (currentBuild.result == 'FAILURE'){
         mail to: 'rishabap23@gmail.com',
         subject: "Pipeline Failure: ${currentBuild.fullDisplayName}",
         body: "The build ${currentBuild.fullDisplayName} failed. Please check the logs for details."
